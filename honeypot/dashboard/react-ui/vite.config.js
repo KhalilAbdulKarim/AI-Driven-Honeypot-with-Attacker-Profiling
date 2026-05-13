1091391487e9a9f3e06260801713eb925ac2ea46
Khalil Abdul Karim <khalilabdulkarim@Khalils-MacBook-Air.local>
2026-05-14 01:59:13 +0200

    external IPs fix

--- honeypot/dashboard/react-ui/vite.config.js
+++ honeypot/dashboard/react-ui/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     strictPort : true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5002',
+        target: 'http://35.180.92.38:5002',
         changeOrigin: true,
       }
     }
