2bb22d22a09b44d5e2aad340c24b6604d5e8de37
Khalil Abdul Karim <khalilabdulkarim@Khalils-MacBook-Air.local>
2026-05-27 14:48:40 +0200

    config update

--- honeypot/dashboard/react-ui/vite.config.js
+++ honeypot/dashboard/react-ui/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     strictPort : true,
     proxy: {
       '/api': {
-        target: 'http://13.37.107.205:5002',
+        target: 'http://13.39.106.131:5002',
         changeOrigin: true,
       }
     }
