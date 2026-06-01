5c52777aa18654d317edc662e50667c0c71fb0af
Khalil Abdul Karim <khalilabdulkarim@Khalils-MacBook-Air.local>
2026-06-01 03:48:31 +0200

    suppress paramiko noise from port scanners

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from honeypot.db import init_db
 from honeypot.ssh_server import start_server
 from honeypot.worker import start as start_worker
+import logging
+
+logging.getLogger("paramiko").setLevel(logging.CRITICAL) # suppress paramiko logs
+logging.getLogger("paramiko.transport").setLevel(logging.CRITICAL)
 
 if __name__ == "__main__":
     init_db()
